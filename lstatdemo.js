b488d3f2abb60b31aacd4fb1737d817d2f1aea0a
Shahid Shaikh <rwtc66@gmail.com>
2014-08-22 23:37:00 +0530

    Update lstatdemo.js

--- lstatdemo.js
+++ lstatdemo.js
@@ -1,19 +1,10 @@
 var fs=require("fs");
 var errno=require('slstat');
-var path="/home/unixroot/Desktop/zookeeper1";
+var path="Path to file";
 fs.lstat(path,function(err,stats){
 	if(err)
 	{
 		console.log(errno.getErrorFromCode('ENOENT'));
-		/*if(err.code==="ENOENT")
-		{
-			fs.exists(path,function(exist) {
-		        if(!exist)
-		        	console.log("Path does not exists");
-			});
-		}
-		console.log(err);
-		*/
 	}
 	else
 	{
